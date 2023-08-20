9533b93a6c25048c18e18ef64101f17c3008ca95
Zach McColgan <99850300+McCZach@users.noreply.github.com>
2023-08-20 13:01:44 -0500

    Gather data bits
    Properly receive user's data bits

--- main.cpp
+++ main.cpp
@@ -36,26 +36,43 @@ int main()
 
 int* sender()
 {
-    int bits;
-    int redundantBits;
-    int parity; //1 for even, 0 for odd
+    int bits,
+        redundantBits,
+        parity;     //1 for even, 0 for odd
+
+    int input;
 
 
     cout << "How many data bits?" << endl;
     cin >> bits;
-
     while (pow(2, redundantBits) < (bits + redundantBits + 1))
     {
         redundantBits += 1;
     }
+    int totalBits = bits + redundantBits;
+    int* set = new int[totalBits];
 
-    cout << redundantBits << endl;
+    cout << "What is the parity? (1 for even/0 for odd)" << endl;
+    cin >> parity;
 
-    int* set = new int[bits + redundantBits];
+    int count = 0;
+    for (int i = totalBits; i > 0; i--)
+    {
+        if ((i & (i - 1)) != 0)
+        {
+            cout << "What is the bit to be transmitted? D" << i << endl;
+            cin >> input;
+            set[i] = input;
+        }
+    }
 
+    cout << "REVERSE" << endl;
+    for (int i = totalBits; i > 0; i--)
+    {
+        cout << set[i] << endl;
+    }
 
     return set;
-
 }
 
 int* receiver()
